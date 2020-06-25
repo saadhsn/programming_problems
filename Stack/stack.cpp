1b03b4dc16df1fea38d7c64eb7bef162b54dad05
Saad <saadhussain_91@hotmail.com>
2020-06-26 00:48:20 +0500

    Formatted file stack.cpp

--- Stack/stack.cpp
+++ Stack/stack.cpp
@@ -2,95 +2,93 @@
 using namespace std;
 
 class Stack {
-	int top;
-	int * stack;
-	int size;
-
-	public:
-		Stack(int max);
-		void create_Stack();
-		void push(int x);
-		int peek();
-		int pop();
-		bool isEmpty();
-		bool isFull();
-		void displayStack();
+    int top;
+    int * stack;
+    int size;
+    
+public:
+    Stack(int max);
+    void createStack();
+    void push(int x);
+    int peek();
+    int pop();
+    bool isEmpty();
+    bool isFull();
+    void displayStack();
 };
 
 Stack::Stack(int max):size(max),top(-1){}
 
-void Stack::create_Stack(){
-	stack = new int[size];
-	cout<<"stack created of lentgh : "<<size<<endl;
+void Stack::createStack(){
+    stack = new int[size];
+    cout<<"Stack created of size : "<<size<<endl;
 }
-//isEmpty() and isFull() can be optimized more
-//see the implementation in geeks for geeks
+
 bool Stack::isEmpty(){
-	if(top == -1)
-		return true;
-	else 
-		return false;
+    return (top == -1);
 }
+
 bool Stack::isFull(){
-	if (top == size-1)
-		return true;
-	else 
-		return false;
+    return (top == size - 1);
 }
+
 void Stack::push(int x){
-	if(isFull()){
-		cout<<"Stack overflow"<<endl;
-	}
-	else {
-		stack[++top]=x;
-	}
+    if(isFull()){
+        cout<<"Stack overflow"<<endl;
+    }
+    else {
+        stack[++top] = x;
+    }
 }
 
 int Stack::peek(){
-	if(isEmpty()){
-		cout<<"Stack is empty"<<endl;
-		return 0;
-	}
-	else {
-		cout<<"element at top : "<<stack[top]<<endl;
-		return stack[top];
-	}
+    if(isEmpty()){
+        cout<<"Stack is empty"<<endl;
+        return 0;
+    }
+    else {
+        cout<<"Element at top : "<<stack[top]<<endl;
+        return stack[top];
+    }
 }
 int Stack::pop(){
-	if(isEmpty()){
-		cout<<"Stack is empty"<<endl;
-		return 0;
-	}
-	else {
-		cout<<"element popped : "<<stack[top]<<endl;
-		return stack[top--];
-	}
+    if(isEmpty()){
+        cout<<"Stack is empty"<<endl;
+        return 0;
+    }
+    else {
+        cout<<"Element popped : "<<stack[top]<<endl;
+        return stack[top--];
+    }
 }
+
 void Stack::displayStack(){
-	if(isEmpty()){
-		cout<<"Stack is empty"<<endl;
-	}
-	else{
-		for(int i=top;i>=0;i--)
-			cout<<stack[i]<<endl;
-	}
+    if(isEmpty()){
+        cout<<"Stack is empty"<<endl;
+    }
+    else{
+        for(int i = top; i >= 0; i--) {
+            cout<<stack[i]<<endl;
+        }
+    }
 }
 
 //Driver program
 int main () {
-	int x;
-	cout<<"Enter the size of stack : "<<endl;
-	cin>>x;
-	Stack s(x);
-	s.create_Stack();
-	s.push (1);
-	s.push (2);
-	s.push (3);
-	s.push (4);
-	s.push (5);
-	s.peek();
-	s.pop();
-	s.peek();
-	s.displayStack();
+
+    int x ,i;
+    cout<<"Enter the size of stack : "<<endl;
+    cin>>x;
+    Stack s(x);
+    s.createStack();
+    for (i = 0; i < x; i++){
+        s.push(i+1);
+    }
+    s.peek();
+    s.pop();
+    s.peek();
+    s.displayStack();
+
+    return 0;
 
 }
